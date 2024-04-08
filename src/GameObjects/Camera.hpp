cfa8f27552672a5f2905cf1c8a667b8b3ad3cd5a
NULL the High Priest <el.rovisoft@gmail.com>
2024-04-08 22:38:52 +0300

    0.1.4a: minor fixes

--- src/GameObjects/Camera.hpp
+++ src/GameObjects/Camera.hpp
@@ -12,43 +12,35 @@ namespace game_objects {
 	public:
 		sf::Vector2f get_size() const { return this->m_view.getSize(); }
 		void set_size(float width, float height) { this->m_view.setSize(width, height); }
-		sf::Vector2f get_position() const { return this->m_view.getCenter(); }
-		void set_position(float x, float y) { this->m_view.setCenter(x, y); }
+		sf::Vector2f get_position() const { return this->m_position; }
+		void set_position(float x, float y) {
+			this->m_position = { x, y };
+			this->_update_position();
+		}
 
 		void move(float x, float y) { this->m_offset = { x, y }; }
 		
 		const sf::View& view() const { return this->m_view; }
 
-		//TODO: every 1x + 0.5 steps position of tiles has white span
 		virtual void update() {
-			auto position = this->m_view.getCenter() + this->m_offset;
-			/*auto dx = std::fabs(std::fmodf(position.x, 1.f)), dy = std::fabs(std::fmodf(position.y, 1.f));
-			if (dx >= 0.48f && dx <= 0.52f) {
-				position.x += this->min_sc;
-				this->m_x_sc = true;
-			} else if (this->m_x_sc) {
-				position.x -= this->min_sc;
-				this->m_x_sc = false;
-			}
-			if (dx >= 0.48f && dx <= 0.52f) {
-				position.y += this->min_sc;
-				this->m_y_sc = true;
-			} else if (this->m_y_sc) {
-				position.x -= this->min_sc;
-				this->m_y_sc = false;
-			}*/
-
-			this->m_view.setCenter(position);
+			this->m_position += this->m_offset;
+			this->_update_position();
 			this->m_offset = { 0, 0 };
 		}
 		virtual void render() = 0;
 
 	protected:
-		//position is center, offset is for drawing
-		/*const float min_sc = std::powf(10, -5);
-		bool m_x_sc = false, m_y_sc = false;*/
 		sf::Vector2f m_position, m_offset;
 		sf::View m_view;
+
+	private:
+		void _update_position() {
+			if (std::fmodf(this->m_position.x, 1.f) != 0)
+				this->m_position.x = std::roundf(this->m_position.x);
+			if (std::fmodf(this->m_position.y, 1.f) != 0)
+				this->m_position.y = std::roundf(this->m_position.y);
+			this->m_view.setCenter(this->m_position);
+		}
 	};
 	using ICamera = std::shared_ptr <BaseCamera>;
 }
