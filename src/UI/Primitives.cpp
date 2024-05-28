298d0d3875d4d848f7a57ba2c1036332f1bda160
El_Rovi Soft <el.rovisoft@gmail.com>
2024-05-28 10:48:01 +0300

    0.3.0: refactoring started

--- src/UI/Primitives.cpp
+++
@@ -1,8 +0,0 @@
-#include "UI/Primitives.hpp"
-
-namespace util {
-	sf::Color to_color(const toml::value& value) {
-		auto colors = toml::get <std::vector <sf::Uint8>>(value);
-		return sf::Color(colors[0], colors[1], colors[2], colors.size() == 4 ? colors[3] : 255);
-	}
-}
